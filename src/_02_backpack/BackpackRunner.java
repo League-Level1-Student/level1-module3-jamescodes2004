ec980d84a1ae06137c284d2dfa6d3abe84cdb0ad
League Student <league@iMac-29.attlocal.net>
2019-08-06 19:55:24 -0700

    8/6/19

--- src/_02_backpack/BackpackRunner.java
+++ src/_02_backpack/BackpackRunner.java
@@ -2,6 +2,9 @@ package _02_backpack;
 
 public class BackpackRunner {
 	public static void main(String[] args) {
+		Backpack b = new Backpack();
 		new Backpack().packAndCheck();
+
+		b.goToSchool();
 	}
 }
